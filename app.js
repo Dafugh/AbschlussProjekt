a45ee80f615c4cec013a0d4dac80a935a879fe2c
Dafugh <jonathan.becker96@gmail.com>
2019-05-14 13:36:16 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,102 @@
+//Get NodeJs modules
+var express = require('express');
+var app = express();
+var http = require('http');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+var cookieParser = require('cookie-parser');
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.get('/', (request, response) =>  response.sendFile(`${__dirname}/Index/index.html`));
+app.use('/', express.static(__dirname + '/index'));
+app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/template', express.static(__dirname + '/template'));
+app.use('/views',express.static(__dirname +'/views'));
+app.use('/index',express.static(__dirname +'/Index'));
+
+function createId() { //Six digit number for the UID of the room
+    var randomNumber = Math.random();
+    var  decimaluid= randomNumber *100000;
+    var uid = Math.trunc(decimaluid);
+    return uid;
+} 
+var database = mysql.createConnection({ 
+    host: "localhost",
+    port: "3309",
+    user: "projektadmin",
+    password: "adminprojekt",
+    database: "poker"
+})
+function insertIdToDatabase(id) {
+    database.query("INSERT INTO `Room` VALUES ('"+id+"')", function(err, res) {
+         if(err) throw err;
+         console.log("Successfully inserted id " + id);
+    })
+}
+function insertUserName(username,id) {
+    database.query("INSERT INTO `Users` VALUES ('"+username+"','"+id+"')", function(err, res) {
+        if(err) throw err;
+        console.log("Successfully inserted User " + username + " on roomid " + id);
+   })
+}
+app.post('/Index/data', (req, res) => {
+    const roomId = createId();
+    const username = req.body.Input_UserName;
+    insertIdToDatabase(roomId);
+    insertUserName(username,roomId); 
+    res.redirect(`/room/${roomId}/${username}`);
+});
+app.get('/room/:id', (req, res) => {
+    var id = {
+        id : req.params.id
+    }
+    res.cookie('ID',id);
+    res.sendFile(`${__dirname}/Index/roomconnect.html`);
+});
+app.post('/Index/join', (req, res) => {
+    const username = req.body.Input_UserNameJoin;
+    const roomId = req.cookies.ID.id;
+    if(username != "undefined" && roomId !="undefined") insertUserName(username,roomId);
+    res.redirect(`/room/${roomId}/${username}`);
+});
+app.get('/room/:id/:username', (req, res) => {
+    const id = req.params.id;
+    res.sendFile(`${__dirname}/template/roomtemplate.html`, function(err, res) {
+
+    });
+    io.on('connection', function(socket) {
+        console.log("ID: " + socket.id);
+        database.query("INSERT INTO Votes(Username, room_id) VALUES('"+req.params.username+"', '"+req.params.id+"') ON DUPLICATE KEY UPDATE Username = '"+req.params.username+"', room_id = '"+req.params.id+"'", function(err, res) {
+            if(err) throw err;
+            database.query("SELECT * FROM `Votes` WHERE room_id = '"+id+"'", function(error, result) {
+                if(error) throw error;
+                console.log(result);
+                io.sockets.emit('Table', result);
+           })
+       })
+        socket.on('card_Choosen', function(data){
+            console.log("2 ID: " + data.vote);
+            if(req.params.id == data.roomid) {
+                database.query("INSERT INTO Votes VALUES('"+data.username+"','"+data.vote+"','"+data.roomid+"')ON DUPLICATE KEY UPDATE Vote = '"+data.vote+"'", function(err, res) {
+                    if(err) throw err;
+                    console.log("Successfully inserted Vote: " + data.vote + " on roomid " + data.roomid);
+                    database.query("SELECT * FROM `Votes` WHERE room_id = '"+id+"'", function(error, result) {
+                        if(error) throw error;
+                        io.sockets.emit('Table', result);
+                    })
+                })
+            }
+        })
+    })
+   
+})
+server.listen(3000);
+
---
+++ js/template.js
@@ -0,0 +1,77 @@
+var socket = io('http://localhost:3000/');
+var url = window.location.href;
+var urlSplit = url.split("/");
+var roomname = urlSplit[4];
+var currentusername = urlSplit[5];
+document.getElementById('cardSet_1').addEventListener('click', card1);
+document.getElementById('cardSet_2').addEventListener('click', card2);
+document.getElementById('cardSet_3').addEventListener('click', card3);
+document.getElementById('cardSet_4').addEventListener('click', card4);
+document.getElementById('cardSet_5').addEventListener('click', card5);
+document.getElementById('cardSet_6').addEventListener('click', card6);
+document.getElementById('cardSet_7').addEventListener('click', card7);
+document.getElementById('cardSet_8').addEventListener('click', card8);
+document.getElementById('cardSet_9').addEventListener('click', card9);
+document.getElementById('cardSet_10').addEventListener('click', card10);
+document.getElementById('cardSet_11').addEventListener('click', card11);
+document.getElementById('cardSet_12').addEventListener('click', card12);
+document.getElementById('cardSet_13').addEventListener('click', card13);
+function card1() {
+ socket.emit('card_Choosen',{vote: "0", });
+}
+function card2() {
+    socket.emit('card_Choosen',{vote: "0.5", username: currentusername, roomid: roomname});
+}
+function card3() {
+    socket.emit('card_Choosen',{vote: "1", username: currentusername, roomid: roomname});
+}
+function card4() {
+    socket.emit('card_Choosen',{vote: "2", username: currentusername, roomid: roomname});
+}
+function card5() {
+    socket.emit('card_Choosen',{vote: '3', username: currentusername, roomid: roomname});
+}
+function card6() {
+    socket.emit('card_Choosen',{vote: '5', username: currentusername, roomid: roomname});
+}
+function card7() {
+    socket.emit('card_Choosen',{vote: '8', username: currentusername, roomid: roomname});
+}
+function card8() {
+    socket.emit('card_Choosen',{vote: '13', username: currentusername, roomid: roomname});
+}
+function card9() {
+    socket.emit('card_Choosen',{vote: '20', username: currentusername, roomid: roomname});
+}
+function card10() {
+    socket.emit('card_Choosen',{vote: '40', username: currentusername, roomid: roomname});
+}
+function card11() {
+    socket.emit('card_Choosen',{vote: '100', username: currentusername, roomid: roomname});
+}
+function card12() {
+    socket.emit('card_Choosen',{vote: '?', username: currentusername, roomid: roomname});
+}
+function card13() {
+    socket.emit('card_Choosen',{vote: 'Pause', username: currentusername, roomid: roomname});
+}
+socket.on('Table', function (data) {
+    //console.log("serverroom: " + data[0].room_id+ " roomname: " + roomname);
+    if(data[0].room_id == roomname) {
+        document.getElementById('User').innerHTML = insertUsers(data);
+    }
+});
+function insertUsers(data) {
+    var s = '<tr id="tabletop"><th id="tableInfoHeadN">Name</th><th id="tableInfoHeadV">Vote</th></tr>';
+    for (var i = 0; i<data.length; i++) {
+        s += "<tr>";
+        s += "<td>";
+        s += data[i].Username;
+        s += "</td>";
+        s += "<td>"; 
+        s += data[i].Vote;
+        s += "</td>";
+        s += "</tr>";
+    }
+ return s;
+}
